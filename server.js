ec407a1ac665387968296681d2698651a633cd85
Geovani Rodrigues <geovanni3344@outlook.com>
2026-04-17 20:27:13 -0300

    fix: adiciona nova URL do frontend ao CORS do backend

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());  
 app.use(cors({
   origin: ['http://localhost:5173',
-  'https://superfrango.vercel.app'],
+  'https://superfrangoapi.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
